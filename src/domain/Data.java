04071ae91b94b321d74d5ff4d82edce5e23fdec5
Cássio Rodrigues <curriculocassio@hotmail.com>
2023-04-04 19:14:04 -0300

    etapa 1

---
+++ src/domain/Data.java
@@ -0,0 +1,91 @@
+package domain;
+
+public class Data {
+    private int dia;
+    private int mes;
+    private int ano;
+
+    public Data(int dia, int mes, int ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+
+        validarData();
+    }
+
+    private void validarData() {
+        boolean dataValida = true;
+
+        if (mes < 1 || mes > 12) {
+            dataValida = false;
+        } else if (dia < 1 || dia > diasNoMes()) {
+            dataValida = false;
+        }
+
+        if (!dataValida) {
+            System.out.println("Data inválida. Será atribuída a data padrão: 1/1/2000");
+            this.dia = 1;
+            this.mes = 1;
+            this.ano = 2000;
+        }
+    }
+
+    private int diasNoMes() {
+        int diasNoMes = 0;
+
+        switch (mes) {
+            case 2:
+                diasNoMes = (verificaAnoBissexto()) ? 29 : 28;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                diasNoMes = 30;
+                break;
+            default:
+                diasNoMes = 31;
+        }
+
+        return diasNoMes;
+    }
+
+    public boolean verificaAnoBissexto() {
+        if (ano % 4 == 0 && (ano % 100 != 0 || ano % 400 == 0)) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    @Override
+    public String toString() {
+        return dia + "/" + mes + "/" + ano;
+    }
+}
---
+++ src/domain/Loja.java
@@ -0,0 +1,68 @@
+package domain;
+
+public class Loja {
+    private String nome;
+    private int quantidadeFuncionarios;
+    private double salarioBaseFuncionario;
+
+    public Loja(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario) {
+        this.nome = nome;
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+        this.salarioBaseFuncionario = salarioBaseFuncionario;
+    }
+
+    public Loja(String nome, int quantidadeFuncionarios) {
+        this.nome = nome;
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+        this.salarioBaseFuncionario = -1;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidadeFuncionarios() {
+        return quantidadeFuncionarios;
+    }
+
+    public void setQuantidadeFuncionarios(int quantidadeFuncionarios) {
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+    }
+
+    public double getSalarioBaseFuncionario() {
+        return salarioBaseFuncionario;
+    }
+
+    public void setSalarioBaseFuncionario(double salarioBaseFuncionario) {
+        this.salarioBaseFuncionario = salarioBaseFuncionario;
+    }
+
+    public double gastosComSalario() {
+        return this.salarioBaseFuncionario == -1 ? -1 : this.quantidadeFuncionarios * this.salarioBaseFuncionario;
+    }
+
+    public char tamanhoDaLoja(){
+        if(this.quantidadeFuncionarios < 10){
+            return 'P';
+        }
+
+        if (this.quantidadeFuncionarios <= 30) {
+            return 'M';
+        }
+
+        return 'G';
+    }
+
+    @Override
+    public String toString() {
+        return "Loja{" +
+                "nome='" + nome + '\'' +
+                ", quantidadeFuncionarios=" + quantidadeFuncionarios +
+                ", salarioBaseFuncionario=" + salarioBaseFuncionario +
+                '}';
+    }
+}
